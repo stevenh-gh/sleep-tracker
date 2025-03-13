3dd374d40212381128703fcfd9d58e0d00296878
stevenh-gh <steven.h@live.com>
2025-03-12 22:07:41 -0700

    add website title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,23 @@ import About from "./pages/About.tsx";
 import Contact from "./pages/Contact.tsx";
 import Login from "./pages/Login.tsx";
 import Signup from "./pages/Signup.tsx";
+import { useEffect } from "react";
 
-const App = () => (
-  <Routes>
-    <Route path={"/"} element={<Home/>}/>
-    <Route path={"/about"} element={<About/>}/>
-    <Route path={"/contact"} element={<Contact/>}/>
-    <Route path={"/login"} element={<Login/>}/>
-    <Route path={"/signup"} element={<Signup/>}/>
-  </Routes>
-);
+const App: React.FC = () => {
+
+  useEffect(() => {
+    document.title = "Restwise";
+  }, []);
+
+  return (
+    <Routes>
+      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/about"} element={<About/>}/>
+      <Route path={"/contact"} element={<Contact/>}/>
+      <Route path={"/login"} element={<Login/>}/>
+      <Route path={"/signup"} element={<Signup/>}/>
+    </Routes>
+  );
+};
+
 export default App;
